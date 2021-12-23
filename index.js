4930ebc1c3498f15b28a0f5c92e95eeab30812f4
Loren Kuich <loren.jk3@gmail.com>
2021-12-23 16:04:47 +0300

    Moved routes into lib, cleaned up index a bit more

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
 const express = require('express');
 
-const {
-  io,
-  socketPickup,
-  app,
-  server,
-  renderJsj
-} = require('./lib/jhj');
-
-const routes = require('./routes');
+const { app, server, runServer } = require('./lib/jhj');
 
 app.use(express.static('src/public'));
 
 server.listen(3000, () => {
   console.log("Server started on port 3000");
   
-  let globalBlocks = [];
-
-  for (const { route, file } of routes) {
-    app.get(route, async (req, res) => {
-      const { page, codeBlocks } = await renderJsj(file);
-
-      globalBlocks = codeBlocks;
-
-      res.send(page);
-    });
-  }
-
-  io.on('connection', (socket) =>
-    socketPickup(socket, globalBlocks)
-  );
+  runServer();
 });
